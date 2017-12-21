b326fb8aa702f3d3221abe8244e5b4d5611eb3e5
voona <voona@Teklysium_PC>
2017-12-21 16:50:44 +0530

    Second Commit

---
+++ JavaDBSample/src/vck/servlet/DataTranslator.java
@@ -0,0 +1,40 @@
+package vck.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DataTranslator
+ */
+@WebServlet(description = "Moving Data", urlPatterns = { "/MyPage" })
+public class DataTranslator extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		String name = request.getParameter("name");
+		int age = Integer.parseInt(request.getParameter("age"));
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<h1>"+"Name =" + name + "<br/>");
+		out.println("Age ="+ age + "</h1>");
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
